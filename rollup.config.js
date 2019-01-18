34e069cd67859d865e2adc13eb54739795f201fa
Rahul Kadyan <hi@znck.me>
2019-01-19 02:33:43 +0530

    chore: Add SSR build to "main" and browser build to "module" field

---
+++ rollup.config.js
@@ -0,0 +1,40 @@
+import vue from 'rollup-plugin-vue'
+import babel from 'rollup-plugin-babel'
+import pkg from './package.json'
+
+function version(name) {
+  return `${name} v${require(`${name}/package.json`).version}`
+}
+
+const banner = `
+/**
+ * ${pkg.name} v${pkg.version}
+ * (c) ${new Date().getFullYear()} ${pkg.author.name} <${pkg.author.email}> (${pkg.author.url})
+ * @license ${pkg.license}
+ * @description Built with ${version('rollup-plugin-vue')} and ${version('vue-template-compiler')}
+ */`.trim()
+
+export default [
+  {
+    input: 'Hydrate.vue',
+    plugins: [vue({ template: { optimizeSSR: true } }), babel()],
+    output: [
+      {
+        banner,
+        format: 'cjs',
+        file: pkg.main,
+      },
+    ],
+  },
+  {
+    input: 'Hydrate.vue',
+    plugins: [vue(), babel()],
+    output: [
+      {
+        banner,
+        format: 'es',
+        file: pkg.module,
+      },
+    ],
+  },
+]
