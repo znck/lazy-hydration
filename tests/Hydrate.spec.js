9cc8da2c1d8162bbff6b929494a14032a60a79a4
Rahul Kadyan <hi@znck.me>
2019-01-20 03:16:38 +0530

    test: WIP - A stab on hydration testing

---
+++ tests/Hydrate.spec.js
@@ -0,0 +1,40 @@
+import Vue from 'vue'
+import { renderToString } from '@vue/server-test-utils'
+import { shallowMount } from '@vue/test-utils'
+import Hydrate from '../Hydrate.vue'
+
+function createComponent(onHydration) {
+  return {
+    mounted() {
+      onHydration && onHydration()
+    },
+    render(h) {
+      return h('p', ['Some content!'])
+    },
+  }
+}
+
+function getSlot(fn) {
+  return {
+    slots: { default: createComponent(fn) },
+  }
+}
+
+it('hydrates if no server rendered content is found', done => {
+  const fn = jest.fn()
+  const wrapper = shallowMount(Hydrate, getSlot(fn))
+
+  expect(wrapper.html()).not.toEqual(
+    expect.stringContaining('<p>Some content!</p>')
+  )
+
+  Vue.nextTick(() => {
+    expect(wrapper.html()).toEqual(
+      expect.stringContaining('<p>Some content!</p>')
+    )
+
+    done()
+  })
+})
+
+// TODO: Test hydration.
